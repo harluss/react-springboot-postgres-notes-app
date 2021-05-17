62b2a4848ea3cf91b42e1cc1526d56ac23ba06fd
harluss <26788867+harluss@users.noreply.github.com>
2021-05-18 00:41:24 +0100

    updated it tests to check created and updated at, added test dataset, updated profiles in application yml

--- src/main/resources/db/changeset/012-notes-dataset.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
---changeset harluss:1
-INSERT INTO notes (title, details, is_pinned, created_at, updated_at)
-VALUES
-    ('some title', 'some random text, nothing special', true, NOW(), NOW()),
-    ('another title', 'again, nothing special', false, NOW(), NOW()),
-    ('yet title again', 'and again, nothing, nothing at all', false, NOW(), NOW())
